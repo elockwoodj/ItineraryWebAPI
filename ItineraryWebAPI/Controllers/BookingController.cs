2aaa6d49c2cb4398f1239cfc13b0f01a4502fff0
a9027410 <a9027410@9309-32J522070>
2018-04-12 13:44:06 +0100

    controllers updated

--- ItineraryWebAPI/Controllers/BookingController.cs
+++ ItineraryWebAPI/Controllers/BookingController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using System.Net.Http.Headers;
+
 
 namespace ItineraryWebAPI.Controllers
 {
