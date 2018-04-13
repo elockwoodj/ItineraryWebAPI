0f05d0cb9875482ef4a7cdf575cae707a3da59b2
Edward Johnson <EdvardoJohnson@hotmail.co.uk>
2018-04-13 14:33:20 +0100

    accountId added to login

--- ItineraryWebAPI/Controllers/LoginController.cs
+++ ItineraryWebAPI/Controllers/LoginController.cs
@@ -23,23 +23,23 @@ namespace ItineraryWebAPI.Controllers
             if (myAuth.username == "mark201" && myAuth.password == "notsure")
             {
                 myAuth.authenticated = true;
-                myAuth.role = 1;
+                myAuth.accountId = 1;
                 myAuth.name = "Mark Godson";
 
                 HttpResponseMessage response =
                 Request.CreateResponse(HttpStatusCode.Accepted, myAuth);
-                response.Headers.Location = new Uri(Request.RequestUri, "/api/login/" + myAuth.role.ToString());
+                response.Headers.Location = new Uri(Request.RequestUri, "/api/login/" + myAuth.accountId.ToString());
                 return response;
 
             }
             else if (myAuth.username == "emma103" && myAuth.password == "prettysure")
             {
                 myAuth.authenticated = true;
-                myAuth.role = 2;
+                myAuth.accountId = 2;
                 myAuth.name = "Emma Smith";
                 HttpResponseMessage response =
                 Request.CreateResponse(HttpStatusCode.Accepted, myAuth);
-                response.Headers.Location = new Uri(Request.RequestUri, "/api/login/" + myAuth.role.ToString());
+                response.Headers.Location = new Uri(Request.RequestUri, "/api/login/" + myAuth.accountId.ToString());
                 return response;
 
                 //                return 2;
@@ -57,7 +57,6 @@ namespace ItineraryWebAPI.Controllers
             else
             {
                 myAuth.authenticated = false;
-                myAuth.role = 0;
                 myAuth.name = "";
                 HttpResponseMessage response =
                     Request.CreateResponse(HttpStatusCode.OK, myAuth);
@@ -72,7 +71,7 @@ namespace ItineraryWebAPI.Controllers
         public string password;
         public string name;
         public bool authenticated;
-        public int role;
+        public int accountId;
     }
     }
 //}
